ad019d5166fbfd720042bbc91e9339e737910dcb
Atma1 <atmasusanto08@gmail.com>
2024-06-15 12:43:49 +0800

    moved questionRadio into its own file

--- src/app/components/Question.jsx
+++ src/app/components/Question.jsx
@@ -1,56 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import {decode} from 'html-entities';
+import QuestionRadio from './QuestionRadio';
 
 export default function Questions(props) {
-  const QuestionRadio = (props) => {
-    const selected = props.selectedAnswerIdx == props.idx;
-    const isCorrect = props.content == props.correctAnswer;
-    let color;
-    let border;
-    let opacity;
-
-    if (selected && !props.endGame) {
-      color = '#D6DBF5';
-      border = 'none';
-    } else if (props.endGame && isCorrect) {
-      color = '#94D7A2';
-      border = 'none';
-    } else if (selected && props.endGame && !isCorrect) {
-      color = '#F8BCBC';
-      border = 'none';
-    } else {
-      'white';
-    }
-
-    if (props.endGame && !isCorrect) {
-      opacity = '70%';
-    }
-
-    const styles = {
-      background: color,
-      border: border,
-      opacity: opacity,
-    };
-
-    return (
-      <>
-        <div className="question-radio" style={styles}>
-          <label style={styles} className='question-radio-label'>
-            <input
-              type='radio'
-              value={decode(props.content)}
-              checked={selected}
-              onChange={() => props.handleClick(props.questionId, props.idx)}
-              name={props.questionId}
-              className='question-radio-button'
-            />
-            {decode(props.content)}
-          </label>
-        </div>
-      </>);
-  };
-
   const questionsRadio = [];
   for (let idx = 0; idx < props.questions.length; idx++) {
     questionsRadio.push(
@@ -76,4 +28,4 @@ export default function Questions(props) {
       </div>
     </div>
   );
-}
+};
